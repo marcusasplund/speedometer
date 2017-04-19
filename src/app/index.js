e90ae04d3fdfcb57fe6f83835847987480109d6f
Marcus Asplund <rootfood@gmail.com>
2017-04-19 19:36:55 +0200

    :star: :tada: js fix

--- src/app/index.js
+++ src/app/index.js
@@ -1,40 +1,41 @@
 
 import {registerWorker} from './register-worker'
-const speed = document.getElementById('speed');
-const unitEl = document.getElementById('unit');
+const speed = document.getElementById('speed')
+const unitEl = document.getElementById('unit')
 
-let index = 0;
+let index = 0
 const units = [{
-  'factor' : 3.6,
-  'unit' : 'km/h'
-},{
-  'factor' : 1.94384449,
-  'unit' : 'knots'
-},{
-  'factor' : 2.23693629,
-  'unit' : 'mph'
-},{
-  'factor' : 1,
-  'unit' : 'm/s'
-}];
+  'factor': 3.6,
+  'unit': 'km/h'
+}, {
+  'factor': 1.94384449,
+  'unit': 'knots'
+}, {
+  'factor': 2.23693629,
+  'unit': 'mph'
+}, {
+  'factor': 1,
+  'unit': 'm/s'
+}]
 
 const options = {
   enableHighAccuracy: true,
-  maximumAge        : 250
+  maximumAge: 250
 }
 
-const success = (e) =>
-  speed.textContent = parseInt(e.coords.speed * units[index].factor);
+const success = (e) => {
+  speed.textContent = parseInt(e.coords.speed * units[index].factor)
+}
 
 const error = (e) => {
-  speed.textContent = e.message;
-  unitEl.textContent = ':-(';
+  speed.textContent = e.message
+  unitEl.textContent = ':-('
 }
 
-navigator.geolocation.watchPosition(success, error, options);
+navigator.geolocation.watchPosition(success, error, options)
 
 const toggleUnit = (e) => {
-  if (e.target.tagname = 'BUTTON') {
+  if (e.target.tagName === 'BUTTON') {
     index = +e.target.id
     unitEl.textContent = units[index].unit
   }
@@ -42,4 +43,4 @@ const toggleUnit = (e) => {
 
 document.body.addEventListener('click', toggleUnit, false)
 
-// registerWorker()
+registerWorker()
